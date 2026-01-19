a01e8a647beb12b42ff90d554f118ae205575187
ajmurphy09 <amandamurphy.1029@gmail.com>
2026-01-19 16:07:04 -0600

    Update 1_pem_reddit_categorize_submissions.py
    add run prompt to top of file

--- 1_pem_reddit_categorize_submissions.py
+++ 1_pem_reddit_categorize_submissions.py
@@ -1,3 +1,8 @@
+##run prompt :
+#python3 ~/extract_cfs_pem.py \
+#  --submissions ~/Downloads/reddit/subreddits24/cfs_submissions.zst \
+#  --comments ~/Downloads/reddit/subreddits24/cfs_comments.zst \
+#  --out ~/Downloads/cfs_clean.parquet
 #!/usr/bin/env python3
 """
 Extract r/CFS submissions + comments from Pushshift-style .zst NDJSON dumps
